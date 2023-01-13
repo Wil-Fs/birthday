014f0a43259247137fdc87be72919bc6a13f0b33
William <williamfs133@gmail.com>
2023-01-12 21:55:12 -0300

    Done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
-console.log("Good luck");
+AOS.init();
+
+const eventDate = new Date("Oct 02, 2023 23:00:00");
+const eventTimeStamp = eventDate.getTime();
+
+const countHours = setInterval(() => {
+    
+    const now = new Date();
+    const actualTimeStamp = now.getTime();
+
+    const timeLeft = eventTimeStamp - actualTimeStamp;
+
+    const dayInMs = 1000 * 60 * 60 * 24;
+    const hoursiMs = 1000 * 60 * 60;
+    const minuteinMs = 1000 * 60;
+
+    const dayUntilEvent = Math.floor(timeLeft / dayInMs);
+    const hoursUntilEvent = Math.floor((timeLeft % dayInMs) / hoursiMs);
+    const minutesUntilEvent = Math.floor((timeLeft % hoursUntilEvent) / minuteinMs);
+    const secondsUntilEvent = Math.floor((timeLeft % minuteinMs) / 1000);
+
+    document.getElementById('time').innerHTML = `${dayUntilEvent} Dias, ${hoursUntilEvent} Horas, ${minutesUntilEvent} Min, ${secondsUntilEvent}s`;
+
+}, 1000);
